3b45a984af0b129fcb59118b0ea81052971da34d
Ifeanyijosephh <josephify304@gmail.com>
2025-07-23 23:51:10 +0100

    -added a new Company Logo -added new colors for better visuals across all pages -removed background for the hero Image for better styling -debugged the entire code and fixed syntax and mispelt words -changed Logo to a new one and images

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
                 const navLinks = document.querySelector(".nav-links");
 
                 hambuger.addEventListener("click", () => {
-                    hamburger.classList.toggle("active");
+                    hambuger.classList.toggle("active");
                     navMenu.classList.toggle("active");
 
                     document.querySelectorALL(".nav-link").forEach(n => n.addEventListener("click", () => {
-                        hamburger.classList.remove("active");
+                        hambuger.classList.remove("active");
                         navMenu.classList.remove("active");
                     }))
-                })
+                });
